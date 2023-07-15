0dc49e29ef10c4d228b0a342a2506e1a09e5132e
TessaNyquist <tessanyquist@outlook.com>
2023-07-15 18:14:51 -0400

    Update Button.js

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,5 +1,7 @@
 import "./Button.css";
 
+//functional component called "Button" using arrow function syntax. 
+//The component takes in three props: className, value, and onClick.
 const Button = ({ className, value, onClick }) => {
   return (
     <button className={className} onClick={onClick}>
